c179f3ea400cb83812bbf6b811d78ecad21eb5eb
John Torres <john.torres.consulting@gmail.com>
2025-02-09 20:41:04 +0100

    adjusting FTTransformer and fixing unpickling issue with torch.load function

--- notebooks/john/pytorch_classify.py
+++ notebooks/john/pytorch_classify.py
@@ -2,6 +2,7 @@ from pytorch_tabular import TabularModel
 from pytorch_tabular.config import DataConfig, OptimizerConfig, TrainerConfig
 from pytorch_tabular.models import (
     FTTransformerConfig,
+    GatedAdditiveTreeEnsembleConfig
 )
 from pytorch_tabular.models.common.heads import LinearHeadConfig
 import os
@@ -9,9 +10,35 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 import torch
+from sklearn.metrics import confusion_matrix, classification_report, f1_score, accuracy_score, log_loss
+
 from omegaconf import DictConfig
-import omegaconf.basecontainer as ContainerMetadata
-torch.serialization.add_safe_globals([DictConfig])
+torch.serialization.safe_globals([DictConfig])
+
+# Adjust Function pytorch_tabular/utils/python_utils.py
+
+# def pl_load(
+#     path_or_url: Union[IO, _PATH],
+#     map_location: _MAP_LOCATION_TYPE = None,
+# ) -> Any:
+#     """Loads a checkpoint.
+
+#     Args:
+#         path_or_url: Path or URL of the checkpoint.
+#         map_location: a function, ``torch.device``, string or a dict specifying how to remap storage locations.
+
+#     """
+#     if not isinstance(path_or_url, (str, Path)):
+#         # any sort of BytesIO or similar
+#         return torch.load(path_or_url, map_location=map_location, weights_only=False)
+#     if str(path_or_url).startswith("http"):
+#         return torch.hub.load_state_dict_from_url(
+#             str(path_or_url),
+#             map_location=map_location,  # type: ignore[arg-type] # upstream annotation is not correct
+#         )
+#     fs = get_filesystem(path_or_url)
+#     with fs.open(path_or_url, "rb") as f:
+#         return torch.load(f, map_location=map_location, weights_only=False)
 
 def train_test_ft_tabular(df_train: pd.DataFrame, 
                           df_test: pd.DataFrame, 
@@ -37,7 +64,7 @@ def train_test_ft_tabular(df_train: pd.DataFrame,
     train, val = train_test_split(df_train, test_size=0.1, random_state=random_seed)
 
     x_test= df_test[features]
-    y_test = df_test[target_col]
+    y_test = df_test[target_col].values
 
     # Config 
     data_config = DataConfig(
@@ -49,7 +76,7 @@ def train_test_ft_tabular(df_train: pd.DataFrame,
     trainer_config = TrainerConfig(
         #     auto_lr_find=True, # Runs the LRFinder to automatically derive a learning rate
         batch_size=32,
-        max_epochs=30,
+        max_epochs=10,
         early_stopping="valid_loss",  # Monitor valid_loss for early stopping
         early_stopping_mode="min",  # Set the mode as min because for val_loss, lower is better
         early_stopping_patience=5,  # No. of epochs of degradation training will wait before terminating
@@ -73,6 +100,13 @@ def train_test_ft_tabular(df_train: pd.DataFrame,
         head="LinearHead",  # Linear Head
         head_config=head_config,  # Linear Head Config
     )
+    
+    # model_config = GatedAdditiveTreeEnsembleConfig(
+    # task="classification",
+    # learning_rate=1e-3,
+    # head="LinearHead",  # Linear Head
+    # head_config=head_config,  # Linear Head Config
+    # )
 
     tabular_model = TabularModel(
         data_config=data_config,
@@ -83,9 +117,30 @@ def train_test_ft_tabular(df_train: pd.DataFrame,
     
         
     tabular_model.fit(train=train, validation=val)
+    y_pred= np.array(tabular_model.predict(test=x_test))[:, -1]  
     
-    eval_metrics = tabular_model.evaluate(test=x_test, target=y_test, metrics=["accuracy", "f1", "roc_auc"])
+      
+    conf_matrix = confusion_matrix(y_test, y_pred)
+    class_report = classification_report(y_test, y_pred)
+    f1_score_result = f1_score(y_test, y_pred)
+    accuracy_score_result = accuracy_score(y_test, y_pred)
     
+    print(f"Acurracy Score: {accuracy_score_result}")   
+    print(f"Classification Report: {class_report}")
+    
+    cv_scores = None
+    #cv_scores = cross_val_score(model, X_train, y_train, cv=cv)
+    print(f"Cross Validation Scores: {cv_scores}")
+    
+    scores = None
+    
+    metrics = {
+        "class_report": class_report,
+        "conf_matrix": conf_matrix,
+        "f1_score": f1_score_result,
+        "accuracy_score": accuracy_score_result
+    }
+  
     return tabular_model, eval_metrics
 
 
