1f61c0f91ee097d5ff2d8a705b072462f1046d5b
John Torres <john.torres.consulting@gmail.com>
2025-02-22 11:54:56 +0100

    update files

--- notebooks/john/FTT_Tabular_optuna.py
+++ notebooks/john/FTT_Tabular_optuna.py
@@ -14,6 +14,7 @@ from sklearn.metrics import confusion_matrix, classification_report, f1_score, a
 
 from omegaconf import DictConfig
 import pickle
+import optuna
 torch.serialization.safe_globals([DictConfig])
 
 # Adjust Function pytorch_tabular/utils/python_utils.py
@@ -47,6 +48,7 @@ def train_test_ft_tabular(df_train: pd.DataFrame,
                           num_cols: list[str], 
                           cat_cols: list[str],
                           target_col: list[str],
+                          n_opt_trials: int = 10,
                           random_seed: int = 123) -> tuple:
    
     train, val = train_test_split(df_train, test_size=0.2, random_state=random_seed)
@@ -56,13 +58,67 @@ def train_test_ft_tabular(df_train: pd.DataFrame,
     
     X_test = df_test.drop(columns=['user_of_latest_model'])  # Features
     y_test = df_test['user_of_latest_model']  # Target variable
-    
-    # Config 
+
     data_config = DataConfig(
-        target=target_col,  # target should always be a list.
-        continuous_cols=num_cols,
-        categorical_cols=cat_cols,
+            target=target_col,  # target should always be a list.
+            continuous_cols=num_cols,
+            categorical_cols=cat_cols,
     )
+    
+    def objective(trial):
+        
+        learning_rate = trial.suggest_float("learning_rate", 1e-5, 1e-1, log=True)
+        num_heads = trial.suggest_categorical("num_heads", [4, 8, 16]) 
+        num_attn_blocks = trial.suggest_int("num_attn_blocks", 1, 4)
+        
+        
+        trainer_config = TrainerConfig(
+            #     auto_lr_find=True, # Runs the LRFinder to automatically derive a learning rate
+            batch_size=32,
+            max_epochs=30,
+            early_stopping="valid_loss",  # Monitor valid_loss for early stopping
+            early_stopping_mode="min",  # Set the mode as min because for val_loss, lower is better
+            early_stopping_patience=5,  # No. of epochs of degradation training will wait before terminating
+            checkpoints="valid_loss",  # Save best checkpoint monitoring val_loss
+                load_best=True,  # After training, load the best checkpoint
+        )
+
+        optimizer_config = OptimizerConfig()
+
+        head_config = LinearHeadConfig(
+            layers="",  # No additional layer in head, just a mapping layer to output_dim
+            dropout=0.1,
+            initialization="kaiming",
+        ).__dict__ 
+            
+        
+        # FT Tabular Transformer
+        model_config = FTTransformerConfig(
+            task="classification",
+            head="LinearHead",  # Linear Head
+            head_config=head_config,  # Linear Head Config
+            learning_rate = learning_rate,
+            num_heads=num_heads,
+            num_attn_blocks=num_attn_blocks,
+        )
+        
+        tabular_model = TabularModel(
+            data_config=data_config,
+            model_config=model_config,
+            optimizer_config=optimizer_config,
+            trainer_config=trainer_config,
+        )
+        tabular_model.fit(train=train, validation=val)
+        
+        y_pred_val = np.array(tabular_model.predict(test=val_x))[:, -1]
+        val_f1_score = round(f1_score(val_y, y_pred_val),4)
+            
+        return -val_f1_score
+    
+    optuna.logging.set_verbosity(optuna.logging.WARNING)
+    study = optuna.create_study(direction="minimize")
+    study.optimize(objective, n_trials=n_opt_trials)
+    best_params = study.best_params
 
     trainer_config = TrainerConfig(
         #     auto_lr_find=True, # Runs the LRFinder to automatically derive a learning rate
@@ -83,15 +139,14 @@ def train_test_ft_tabular(df_train: pd.DataFrame,
         initialization="kaiming",
     ).__dict__ 
 
-
     # FT Tabular Transformer
     model_config = FTTransformerConfig(
         task="classification",
         head="LinearHead",  # Linear Head
         head_config=head_config,  # Linear Head Config
-        learning_rate = 1e-3,
-        num_heads=8,
-        num_attn_blocks=4,
+        learning_rate=best_params['learning_rate'],
+        num_heads=best_params['num_heads'],
+        num_attn_blocks=best_params['num_attn_blocks'],
     )
     
     tabular_model = TabularModel(
@@ -131,11 +186,6 @@ if __name__ == "__main__":
     df_train = pd.read_csv(os.path.join(os.getcwd(),"data/processed/task2_best_model_step3_train_data.csv"))
     df_test = pd.read_csv(os.path.join(os.getcwd(),"data/processed/task2_best_model_step3_test_data.csv"))
 
-    # X_train = df_train.drop(columns=['user_of_latest_model'])  # Features
-    # y_train = df_train['user_of_latest_model']  # Target variable
-    
-    # X_test = df_test.drop(columns=['user_of_latest_model'])  # Features
-    # y_test = df_test['user_of_latest_model']  # Target variable
 
     # OneHot-Encoding for categorical columns
     df_train = pd.get_dummies(df_train, columns=df_train.select_dtypes(include=['object']).columns.to_list(), prefix="_cat", drop_first=True)
@@ -155,5 +205,5 @@ if __name__ == "__main__":
     print(df_results)
     
     # Store results as pkl
-    with open(os.path.join(os.getcwd(), "data/processed/ftt_model_results.pkl"), "wb") as f:
+    with open(os.path.join(os.getcwd(), "data/processed/ftt_model_results_optuna.pkl"), "wb") as f:
         pickle.dump(df_results, f)
